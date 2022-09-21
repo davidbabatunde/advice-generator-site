2dcdef90430b8ee6a9b1cf834e73830d929822e7
Reol <reol0624@gmail.com>
2022-09-21 13:23:22 +0100

    added hover state to button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import "./App.css";
 import divider from "./images/pattern-divider-mobile.svg";
+import desktopDivider from "./images/pattern-divider-desktop.svg";
 import dice from "./images/icon-dice.svg";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
   const getData = async () => {
-    await axios("	https://api.adviceslip.com/advice").then((response) =>
-      setData(response.data.slip)
-    );
+    await axios("	https://api.adviceslip.com/advice")
+      .then((response) => setData(response.data.slip))
+      .catch((error) => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          // console.log(error.response.data);
+          // console.log(error.response.status);
+          // console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the
+          // browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
   };
 
   const [data, setData] = useState(null);
@@ -22,9 +41,16 @@ function App() {
       <article>
         <h1>ADVICE #{data ? data.id : null}</h1>
         <q>{data ? data.advice : <h1>loading...</h1>}</q>
-        <img src={divider} alt="divider" />
+        <img
+          id="divider"
+          src={divider}
+          srcSet={`${divider} 300w, ${desktopDivider} 768w`}
+          alt="divider"
+        />
       </article>
-      <img id="dice" src={dice} onClick={getData} alt="dice" />
+      <div id="diceContainer" onClick={getData}>
+        <img id="dice" src={dice} alt="dice" />
+      </div>
     </section>
   );
 }
